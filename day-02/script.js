7990bb43148ca2088f6cf11b9d36040234666eb4
Eren Gazioglu <pompyproduction@gmail.com>
2022-12-04 18:05:43 +0000

    Solve day 02-2

--- day-02/script.js
+++ day-02/script.js
@@ -5,11 +5,12 @@ const sumAll = arr => arr.reduce((acc, val) => acc + val);
 
 async function solve() {
     function playRound(round) {
-        return [
-            () => 3,
-            () => 6,
-            () => 0
-        ][(round[1] - round[0] + 3) % 3]() + round[1] + 1
+        return [3, 6, 0][ // tie, loss, win
+            (round[1] - round[0] + 3) % 3
+        ] + round[1] + 1
+    }
+    function predict(round) {
+        return (round[0] + [2, 0, 1][round[1]]) % 3
     }
 
     // fetch question
@@ -20,8 +21,18 @@ async function solve() {
     }
     let sum = 0;
     for (let i = 0; i < input.length; i++) {
-        sum += playRound(input[i]);
+        sum += playRound(input[i]); // solution 1
     }
-    console.log(sum)
+    console.log("part 1:", sum)
+
+    sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        sum += playRound([
+            input[i][0],
+            predict(input[i])
+        ])
+        // console.log(predict(input[i]));
+    }
+    console.log("part 2:", sum)
 }
 solve();
